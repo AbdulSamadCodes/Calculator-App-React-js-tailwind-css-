e32213fb8eaebecf0594a8506f3f9f8e3f9b882f
Abdul Samad <samadcom797@gmail.com>
2024-10-05 11:58:24 +0500

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import {React} from 'react';
-import './index.css'
+import '/src/Styles/index.css';
 
 function App() {
-  
+  return <h3 className="text-red-700">My calculator</h3>;
 };
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import App from '/src/App.jsx'
+import '/src/Styles/index.css';
 
 createRoot(document.getElementById('root')).render(
     <App />
